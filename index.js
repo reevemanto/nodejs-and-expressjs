de589b25bd70907cedd244a7cdc23a7065a6fc6e
reevemanto <christianmanto2004@gmail.com>
2026-03-16 02:49:36 +0800

    3.1 - Introduction to Node.js and Express.js

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.static('public'));
+
+// Logging Middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+// Sample data
+let items = ['apple', 'Orange', 'Banana'];
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+// Get items
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+// Add item (using /items for POST as shown in module)
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
+});
+
+// Test error route
+app.get('/error', (req, res, next) => {
+    throw new Error('This is a test error');
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
